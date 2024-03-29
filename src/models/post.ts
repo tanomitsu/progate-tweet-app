fbf063cbf953393194e78e511f308e794eb44a3e
tanomitsu2002 <64388900+tanomitsu@users.noreply.github.com>
2024-03-29 23:46:42 +0900

    feat: 投稿一覧画面にリツイートされた投稿を表示

--- src/models/post.ts
+++ src/models/post.ts
@@ -7,6 +7,20 @@ import {
 
 type PostData = Pick<Post, "content" | "userId">;
 export type PostWithUser = Post & {user: UserWithoutPassword};
+export type PostWithUserAndType = PostWithUser & {
+  type: "Post" | "RetweetedPost";
+};
+
+export const selectPostWithUser = {
+  id: true,
+  content: true,
+  userId: true,
+  createdAt: true,
+  updatedAt: true,
+  user: {
+    select: selectUserColumnsWithoutPassword,
+  },
+} as const;
 
 export const createPost = async (postData: PostData): Promise<Post> => {
   const prisma = databaseManager.getInstance();
@@ -64,24 +78,45 @@ export const getPost = async (postId: number): Promise<PostWithUser | null> => {
   return post;
 };
 
-export const getAllPosts = async (): Promise<PostWithUser[]> => {
+export const getAllPosts = async (): Promise<PostWithUserAndType[]> => {
   const prisma = databaseManager.getInstance();
-  const post = await prisma.post.findMany({
+  const posts = await prisma.post.findMany({
     orderBy: {
       createdAt: "desc",
     },
     select: {
-      id: true,
-      content: true,
-      userId: true,
+      ...selectPostWithUser,
+    },
+  });
+
+  const retweets = await prisma.retweet.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
       createdAt: true,
-      updatedAt: true,
-      user: {
+      post: {
         select: {
-          ...selectUserColumnsWithoutPassword,
+          ...selectPostWithUser,
         },
       },
     },
   });
-  return post;
+
+  // order posts and retweets by createdAt
+  const sortedPosts: PostWithUserAndType[] = [
+    ...posts.map(p => ({...p, typename: "Post" as const})),
+    ...retweets.map(r => ({...r, typename: "Retweet" as const})),
+  ]
+    // Paginationを行わない限りは、このようにfetch後にソートして問題ない
+    .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1))
+    .map(postOrRetweet => {
+      if (postOrRetweet.typename === "Post") {
+        // Posts
+        return {...postOrRetweet, type: "Post" as const};
+      }
+      // Retweeted posts
+      return {...postOrRetweet.post, type: "RetweetedPost" as const};
+    });
+  return sortedPosts;
 };
